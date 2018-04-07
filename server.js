b0fc5c2c3e38a1c9defdad6ec080bd479f3d9863
idaselma3025 <idaselma3025@gmail.com>
2018-04-06 21:34:37 -0500

    third

--- server.js
+++ server.js
@@ -22,9 +22,7 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines
 // Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
 
-mongoose.connect(MONGODB_URI, {
-  useMongoClient: true
-});
+mongoose.connect(MONGODB_URI);
 
 
 app.use(logger("dev"));
